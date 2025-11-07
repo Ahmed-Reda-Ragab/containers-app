755eba99cae4b418ec8ac20bf6a3cc63454278d1
AhmedReda <a.ali@isit-sa.com>
2025-11-07 11:27:30 +0200

    fix login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,156 @@
-@extends('layouts.master')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+<div class="bg-body d-flex flex-column align-items-stretch flex-center rounded-4 w-md-600px p-20">
+    <!--begin::Wrapper-->
+    <div class="d-flex flex-center flex-column flex-column-fluid px-lg-10 pb-15 pb-lg-20">
+        <!--begin::Form-->
+        <form class="form w-100 fv-plugins-bootstrap5 fv-plugins-framework" method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <!--begin::Heading-->
+            <div class="text-center mb-11">
+                <!--begin::Title-->
+                <h1 class="text-dark fw-bolder mb-3">{{ __('Login') }}</h1>
+                <!--end::Title-->
+
+            </div>
+            <!--begin::Heading-->
+
+
+
+            <!--begin::Separator-->
+
+            <!--end::Separator-->
+            <!--begin::Input group=-->
+            <div class="fv-row mb-8 fv-plugins-icon-container">
+                <!--begin::Email-->
+                <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent">
+                <!--end::Email-->
+                <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
+
+                @error('email')
+                <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback">
+                    <strong>{{ $message }}</strong>
+                </div>
+                @enderror
+            </div>
+            <!--end::Input group=-->
+            <div class="fv-row mb-3 fv-plugins-icon-container">
+                <!--begin::Password-->
+                <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent">
+                <!--end::Password-->
+                <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
+            </div>
+            <!--end::Input group=-->
+            <!--begin::Wrapper-->
+            <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+                <div></div>
+                <!--begin::Link-->
+                <a href="#" class="link-primary">Forgot Password ?</a>
+                <!--end::Link-->
+            </div>
+            <!--end::Wrapper-->
+            <!--begin::Submit button-->
+            <div class="d-grid mb-10">
+                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                    <!--begin::Indicator label-->
+                    <span class="indicator-label">Sign In</span>
+                    <!--end::Indicator label-->
+                    <!--begin::Indicator progress-->
+                    <span class="indicator-progress">Please wait...
+                        <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                    <!--end::Indicator progress-->
+                </button>
+            </div>
+            <!--end::Submit button-->
+            <!--begin::Sign up-->
+            <div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?
+                <a href="../../demo1/dist/authentication/layouts/creative/sign-up.html" class="link-primary">Sign up</a>
+            </div>
+            <!--end::Sign up-->
+        </form>
+        <!--end::Form-->
+    </div>
+    <!--end::Wrapper-->
+
+    <div class="d-flex flex-stack px-lg-10 d-none">
+        <!--begin::Languages-->
+        <div class="me-0">
+            <!--begin::Toggle-->
+            <button class="btn btn-flex btn-link btn-color-gray-700 btn-active-color-primary rotate fs-base" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" data-kt-menu-offset="0px, 0px">
+                <img data-kt-element="current-lang-flag" class="w-20px h-20px rounded me-3" src="assets/media/flags/united-states.svg" alt="">
+                <span data-kt-element="current-lang-name" class="me-1">English</span>
+                <i class="ki-duotone ki-down fs-5 text-muted rotate-180 m-0"></i>
+            </button>
+            <!--end::Toggle-->
+            <!--begin::Menu-->
+            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-4 fs-7" data-kt-menu="true" id="kt_auth_lang_menu">
+                <!--begin::Menu item-->
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link d-flex px-5" data-kt-lang="English">
+                        <span class="symbol symbol-20px me-4">
+                            <img data-kt-element="lang-flag" class="rounded-1" src="assets/media/flags/united-states.svg" alt="">
+                        </span>
+                        <span data-kt-element="lang-name">English</span>
+                    </a>
+                </div>
+                <!--end::Menu item-->
+                <!--begin::Menu item-->
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link d-flex px-5" data-kt-lang="Spanish">
+                        <span class="symbol symbol-20px me-4">
+                            <img data-kt-element="lang-flag" class="rounded-1" src="assets/media/flags/spain.svg" alt="">
+                        </span>
+                        <span data-kt-element="lang-name">Spanish</span>
+                    </a>
+                </div>
+                <!--end::Menu item-->
+                <!--begin::Menu item-->
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link d-flex px-5" data-kt-lang="German">
+                        <span class="symbol symbol-20px me-4">
+                            <img data-kt-element="lang-flag" class="rounded-1" src="assets/media/flags/germany.svg" alt="">
+                        </span>
+                        <span data-kt-element="lang-name">German</span>
+                    </a>
+                </div>
+                <!--end::Menu item-->
+                <!--begin::Menu item-->
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link d-flex px-5" data-kt-lang="Japanese">
+                        <span class="symbol symbol-20px me-4">
+                            <img data-kt-element="lang-flag" class="rounded-1" src="assets/media/flags/japan.svg" alt="">
+                        </span>
+                        <span data-kt-element="lang-name">Japanese</span>
+                    </a>
+                </div>
+                <!--end::Menu item-->
+                <!--begin::Menu item-->
+                <div class="menu-item px-3">
+                    <a href="#" class="menu-link d-flex px-5" data-kt-lang="French">
+                        <span class="symbol symbol-20px me-4">
+                            <img data-kt-element="lang-flag" class="rounded-1" src="assets/media/flags/france.svg" alt="">
+                        </span>
+                        <span data-kt-element="lang-name">French</span>
+                    </a>
+                </div>
+                <!--end::Menu item-->
+            </div>
+            <!--end::Menu-->
+        </div>
+        <!--end::Languages-->
+        <!--begin::Links-->
+        <div class="d-flex fw-semibold text-primary fs-base gap-5 ">
+            <a href="../../demo1/dist/pages/team.html" target="_blank">Terms</a>
+            <a href="../../demo1/dist/pages/pricing/column.html" target="_blank">Plans</a>
+            <a href="../../demo1/dist/pages/contact.html" target="_blank">Contact Us</a>
+        </div>
+        <!--end::Links-->
+    </div>
+    <!--end::Footer-->
+</div>
+<div class="container d-none">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -18,9 +167,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -32,9 +181,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -51,19 +200,7 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
                     </form>
                 </div>
             </div>
